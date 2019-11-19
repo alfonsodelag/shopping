dd24e4eba489dc6f728f18f857176e3dc8919cc1
Alfonso De La Guardia <alfonsodelag1@gmail.com>
2019-11-19 18:42:49 +0100

    products.html page created

---
+++ app.js
@@ -0,0 +1,14 @@
+function redirectPage(){
+    window.location.href = "products.html";
+}
+
+
+
+
+function Shopping_cart(product, price, qty, total) {
+    this.product = product;
+    this.price = price;
+    this.qty = qty;
+    this.total = total;
+}
+
