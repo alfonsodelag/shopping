2bb8ff04f4afefd2530adea3f8e90e168b3aec3e
Alfonso De La Guardia <alfonsodelag1@gmail.com>
2019-11-21 12:29:44 +0100

    html and function cleaning

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
  let product = [
     {
-        "name": model1,
+        "name": shirt1,
         "stock": 10,
         "price": 5,
         "qty": "qty",
@@ -57,71 +57,7 @@
     }
 ];
 
-// let changeNumber = parseInt(document.getElementById("number").value, 10)
-// let increment = document.getElementsByClassName("increment-btn");
-// let decrement = document.getElementsByClassName("decrement-btn");
 
-// num=0;
-// function changeQty() {
-//     if(increment-btn.onclick == true) {
-//         num++;
-//     }
-//     if(decrement-btn.onclick == true) {
-//         num--;
-//     }
-// };
-
-// document.getElementById("decrement-button").onclick = increaseProduct();
-// document.getElementById("increment-btn").onclick = decreaseProduct();
-
-// for (let index = 0; index < array.length; index++) {
-//     const element = array[index];   
-// }
-
-// function dropdown() {
-//     document.getElementById("dropdown").classList.toggle("show");
-//   }
-  
-//   // Close the dropdown if the user clicks outside of it
-//   window.onclick = dropdown() {
-//     if (!event.target.matches('.dropbtn')) {
-//       var dropdowns = document.getElementsByClassName("dropdown-content");
-//       var i;
-//       for (i = 0; i < dropdowns.length; i++) {
-//         var openDropdown = dropdowns[i];
-//         if (openDropdown.classList.contains('show')) {
-//           openDropdown.classList.remove('show');
-//         }
-//       }
-//     }
-//   }
-
-// num=0;
-
-function increaseProduct() {
-    let options = []; //Array de inputs
-    options = document.getElementsByClassName("number"); //Inputs por id number
-    for (let i = 0; i < options.length; i++) {
-        if (increaseProduct().clicked) {
-            return false;
-            
-        }
-        if (options[i].value >= 0) {
-            options[i].value++;
-        }
-    } 
-};
-
-
-// function increaseProduct() {
-//     document.getElementById("number").value++;
-// }
-
-// function decreaseProduct() {
-//     if(document.getElementById("number").value > 0) {
-//         document.getElementById("number").value--;
-//     }
-// };
 
 function saveName(){
     let username = document.getElementById("username").value;
@@ -132,10 +68,6 @@ function saveItem(){
     let itemqty = document.getElementById("number").value;
     localStorage.setItem("number", itemqty);
 }
-
-function redirectPage(){
-    window.location.href = "products.html";
-};
 
 
 
@@ -149,23 +81,6 @@ function saveProductPrice() {
     localStorage.setItem("productPrice", productPrice);
 }
 
-
-// function myFunction() {
-//     document.getElementById("myDropdown").classList.toggle("show");
-// }
-
-// window.onclick = function(event) {
-// if (!event.target.matches('.dropbtn')) {
-// var dropdowns = document.getElementsByClassName("dropdown-content");
-// var i;
-// for (i = 0; i < dropdowns.length; i++) {
-//     var openDropdown = dropdowns[i];
-//     if (openDropdown.classList.contains('show')) {
-//     openDropdown.classList.remove('show');
-//     }
-// }
-// }
-// }
 
 
 
