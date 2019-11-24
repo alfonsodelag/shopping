806aeb514698b63849d9d071eb4d89a257f747d9
Alfonso De La Guardia <alfonsodelag1@gmail.com>
2019-11-24 18:14:40 +0100

    cleaning the code

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Our Json for storing data about our products.
-let Products =
+var Products =
 [
     {
         "name": "shirt",
@@ -23,7 +22,6 @@ let Products =
 localStorage.setItem("Products", JSON.stringify(Products));
 
 
-//Function used to move from Box1 to Box2 in the HTML
 function startBox() {
     document.getElementById("box1").style.display = "none";
     document.getElementById("box2").style.display = "inline-block";
@@ -31,8 +29,8 @@ function startBox() {
 }
 
 function placeData () {
-    let object = JSON.parse(localStorage.getItem("Products"));
-    for (let i = 0; i < object.length; i++) {
+    var object = JSON.parse(localStorage.getItem("Products"));
+    for (var i = 0; i < object.length; i++) {
         document.getElementsByTagName("h2")[i].innerHTML=object[i].name+" "+object[i].size[object[i].position];
         document.getElementsByTagName("p")[i*2].innerHTML=object[i].price[object[i].position]+"€";
         document.getElementsByClassName("size-btn")[i].value=object[i].size[object[i].position];
@@ -40,12 +38,11 @@ function placeData () {
     }
 }
 
-
 function changeQty(event)
     {
-        let object = JSON.parse(localStorage.getItem("Products"));
+        var object = JSON.parse(localStorage.getItem("Products"));
         var idevent = event.target.id;
-        for (let i = 0; i < object.length; i++) {
+        for (var i = 0; i < object.length; i++) {
             if (idevent == "plus-"+i && object[i].qty[object[i].position] != object[i].stocks[object[i].position]) {
                 object[i].qty[object[i].position]=object[i].qty[object[i].position]+1;
             }else if(idevent=="minus-"+i && object[i].qty[object[i].position] != 0){
@@ -59,9 +56,9 @@ function changeQty(event)
 
     function changeSize(event)
     {
-        let object = JSON.parse(localStorage.getItem("Products"));
+        var object = JSON.parse(localStorage.getItem("Products"));
         var idevent = event.target.id;
-        for (let i = 0; i < object.length; i++) {
+        for (var i = 0; i < object.length; i++) {
             if (idevent == "size-"+i) {
                 object[i].position=object[i].position+1;
                 if (object[i].position > object[i].size.length-1) {
@@ -75,15 +72,15 @@ function changeQty(event)
 
     function saveToCart()
     {
-        let object = JSON.parse(localStorage.getItem("Products"));
+        var object = JSON.parse(localStorage.getItem("Products"));
         document.getElementById("box2").style.display = "none";
         document.getElementById("box3").style.display = "inline-block";
         var products="";
         var quantity="";
         var total="";
         var grandtotal=0;
-        for (let i = 0; i < object.length; i++) {
-            for (let j = 0; j < object[i].qty.length; j++) {
+        for (var i = 0; i < object.length; i++) {
+            for (var j = 0; j < object[i].qty.length; j++) {
                 if (object[i].qty[j] > 0) {
                     products=products+object[i].name+" "+object[i].size[j]+"<br>";
                     quantity=quantity+object[i].qty[j]+"<br>";
@@ -101,15 +98,15 @@ function changeQty(event)
 
     function changeCurrency()
     {
-        let object = JSON.parse(localStorage.getItem("Products"));
+        var object = JSON.parse(localStorage.getItem("Products"));
         document.getElementById("box2").style.display = "none";
         document.getElementById("box3").style.display = "inline-block";
         var products="";
         var quantity="";
         var total="";
         var grandtotal=0;
-        for (let i = 0; i < object.length; i++) {
-            for (let j = 0; j < object[i].qty.length; j++) {
+        for (var i = 0; i < object.length; i++) {
+            for (var j = 0; j < object[i].qty.length; j++) {
                 if (object[i].qty[j] > 0) {
                     products=products+object[i].name+" "+object[i].size[j]+"<br>";
                     quantity=quantity+object[i].qty[j]+"<br>";
@@ -133,7 +130,6 @@ function returnToPage()
     document.getElementById("box2").style.display = "inline-block";
 }
 
-//This function is used to show you the "Thank you for your purchase" page.
 function finish()
 {
     document.getElementById("box3").style.display = "none";
