6fbfecbd985453c928e7ccaaa51692ffb45ec798
Alfonso De La Guardia <alfonsodelag1@gmail.com>
2019-11-25 17:07:04 +0100

    changeCurrency fixes

--- script.js
+++ script.js
@@ -31,7 +31,11 @@ function placeData () {
     var object = JSON.parse(localStorage.getItem("Products"));
     for (var i = 0; i < object.length; i++) {
         document.getElementsByTagName("h2")[i].innerHTML=object[i].name+" "+object[i].size[object[i].position];
-        document.getElementsByTagName("p")[i*2].innerHTML=object[i].price[object[i].position]+"€";
+        if (document.getElementById("change-currency").value == "Price in Dollars") {
+            document.getElementsByTagName("p")[i*2].innerHTML=object[i].price[object[i].position]+"€";
+        }else{
+            document.getElementsByTagName("p")[i*2].innerHTML=object[i].price[object[i].position]+"$"
+        }   
         document.getElementsByClassName("size-btn")[i].value=object[i].size[object[i].position];
         document.getElementsByClassName("qty-choice")[i].innerHTML=object[i].qty[object[i].position];  
     }
@@ -92,35 +96,35 @@ function changeQty(event)
         document.getElementsByClassName("datos")[0].innerHTML=products;
         document.getElementsByClassName("datos")[1].innerHTML=quantity;
         document.getElementsByClassName("datos")[2].innerHTML=total;
-        document.getElementsByClassName("datos")[3].innerHTML=grandtotal+"€";
+        if (document.getElementById("change-currency").value == "Price in Dollars") {
+            document.getElementsByClassName("datos")[3].innerHTML=grandtotal+"€";
+        }else{
+            document.getElementsByClassName("datos")[3].innerHTML=grandtotal+"$";
+        }   
     }
 
     function changeCurrency()
     {
         var object = JSON.parse(localStorage.getItem("Products"));
-        document.getElementById("box2").style.display = "none";
-        document.getElementById("box3").style.display = "inline-block";
-        var products="";
-        var quantity="";
-        var total="";
-        var grandtotal=0;
-        for (var i = 0; i < object.length; i++) {
-            for (var j = 0; j < object[i].qty.length; j++) {
-                if (object[i].qty[j] > 0) {
-                    products=products+object[i].name+" "+object[i].size[j]+"<br>";
-                    quantity=quantity+object[i].qty[j]+"<br>";
-                    total=total+object[i].qty[j]*object[i].price[j]+"<br>";
-                    grandtotal=grandtotal+object[i].qty[j]*object[i].price[j];
+        for (let i = 0; i < object.length; i++) {
+            for (let j = 0; j < object[i].price.length; j++) {
+                if(document.getElementById("change-currency").value == "Price in Dollars") {
+                    object[i].price[j] = Math.floor(object[i].price[j]*1.11);
+                    document.getElementById("change-currency").value = "Price in Euros";
+
+                }else{
+                    object[i].price[j] = Math.round(object[i].price[j]*0.9);
+                    document.getElementById("change-currency").value = "Price in Dollars";
+
                 }
-            }   
+            
+            }
         }
+        localStorage.setItem("Products", JSON.stringify(object));
+        saveToCart();
+        placeData();
 
-        document.getElementsByClassName("datos")[0].innerHTML=products;
-        document.getElementsByClassName("datos")[1].innerHTML=quantity;
-        document.getElementsByClassName("datos")[2].innerHTML=total;
-        document.getElementsByClassName("datos")[3].innerHTML=grandtotal*1.2+"$";
     }
-
 
 
 function returnToPage()
